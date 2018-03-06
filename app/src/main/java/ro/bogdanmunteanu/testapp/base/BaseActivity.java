ac2fc4257b01877a5c47004645e2c1d843e49fb9
Bogdan Munteanu <bogdanmunteanu25@gmail.com>
2018-03-06 20:35:58 +0200

    Added libraries: Butterknife , RxJava , Picasso Added base layer fragment

--- app/src/main/java/ro/bogdanmunteanu/testapp/base/BaseActivity.java
+++ app/src/main/java/ro/bogdanmunteanu/testapp/base/BaseActivity.java
@@ -9,11 +9,17 @@ import android.support.v7.app.AppCompatActivity;
  * Base activity for all stuff
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         //here we could do some additional stuff before starting the activity (init crashlytics, firebase etc
     }
+
+    /**
+     * Helper method to return different layouts in different activities based on layout id
+     * @return
+     */
+    public abstract int getLayoutId();
 }
